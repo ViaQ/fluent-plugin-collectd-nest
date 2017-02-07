6350d936c4859f3eb44ea11c0dd5bc51b819dd32
Anton Sherkhonov <asherkho@redhat.com>
2017-02-07 22:13:42 +0000

    Fixed unit tests

--- test/out_collectd_unroll.rb
+++ test/out_collectd_unroll.rb
@@ -19,65 +19,34 @@ class CollectdUnrollOutputTest < Test::Unit::TestCase
   def test_rewrite_tag
     d = create_driver %[
       type collectd_unroll
+      add_tag_prefix test_tag
     ]
 
     d.run do
-      d.emit([{
+      d.emit({
         "time" => 1000, "host" => 'host', "interval" => 5,
-        "plugin" => 'plugin', "plugin_instance" => 'plugin_instance',
-        "type" => 'type', "type_instance" => 'type_instance',
+        "plugin" => 'plugin1', "plugin_instance" => 'plugin_instance',
+        "type" => 'type1', "type_instance" => 'type_instance',
         "values" => ['v1', 'v2'], "dsnames" => ['n1', 'n2'], "dstypes" => ['t1', 't2']
-      }])
-      d.emit([{
+      })
+      d.emit({
         "time" => 1000, "host" => 'host', "interval" => 5,
-        "plugin" => 'plugin', "plugin_instance" => '',
-        "type" => 'type', "type_instance" => 'type_instance',
+        "plugin" => 'plugin2', "plugin_instance" => '',
+        "type" => 'type2', "type_instance" => 'type_instance',
         "values" => ['v1', 'v2'], "dsnames" => ['n1', 'n2'], "dstypes" => ['t1', 't2']
-      }])
-      d.emit([{
+      })
+      d.emit({
         "time" => 1000, "host" => 'host', "interval" => 5,
-        "plugin" => 'plugin', "plugin_instance" => 'plugin_instance',
-        "type" => '', "type_instance" => 'type_instance',
+        "plugin" => 'plugin3', "plugin_instance" => 'plugin_instance',
+        "type" => 'type3', "type_instance" => 'type_instance',
         "values" => ['v1', 'v2'], "dsnames" => ['n1', 'n2'], "dstypes" => ['t1', 't2']
-      }])
-      d.emit([{
-        "time" => 1000, "host" => 'host', "interval" => 5,
-        "plugin" => 'plugin', "plugin_instance" => 'plugin_instance',
-        "type" => 'type', "type_instance" => '',
-        "values" => ['v1', 'v2'], "dsnames" => ['n1', 'n2'], "dstypes" => ['t1', 't2']
-      }])
+      })
     end
 
-    assert_equal 4, d.emits.length
-    assert_equal "test_tag.host.plugin.plugin_instance.type.type_instance", d.emits[0][0]
-    assert_equal "test_tag.host.plugin.type.type_instance", d.emits[1][0]
-    assert_equal "test_tag.host.plugin.plugin_instance.type_instance", d.emits[2][0]
-    assert_equal "test_tag.host.plugin.plugin_instance.type", d.emits[3][0]
-  end
-
-  def test_use_timestamp
-    d = create_driver %[
-      type collectd_unroll
-    ]
-    
-    d.run do
-      d.emit([{
-        "time" => 1000, "host" => 'host', "interval" => 5,
-        "plugin" => 'plugin', "plugin_instance" => 'plugin_instance',
-        "type" => 'type', "type_instance" => 'type_instance',
-        "values" => ['v1', 'v2'], "dsnames" => ['n1', 'n2'], "dstypes" => ['t1', 't2']
-      }])
-      d.emit([{
-        "time" => 9999, "host" => 'host', "interval" => 5,
-        "plugin" => 'plugin', "plugin_instance" => '',
-        "type" => 'type', "type_instance" => 'type_instance',
-        "values" => ['v1', 'v2'], "dsnames" => ['n1', 'n2'], "dstypes" => ['t1', 't2']
-      }])
-    end
-    
-    assert_equal 2, d.emits.length
-    assert_equal 1000, d.emits[0][1]
-    assert_equal 9999, d.emits[1][1]
+    assert_equal 3, d.emits.length
+    assert_equal "test_tag.plugin1.type1", d.emits[0][0]
+    assert_equal "test_tag.plugin2.type2", d.emits[1][0]
+    assert_equal "test_tag.plugin3.type3", d.emits[2][0]
   end
 
   def test_normalize_record
@@ -86,15 +55,23 @@ class CollectdUnrollOutputTest < Test::Unit::TestCase
     ]
 
     d.run do
-      d.emit([{
-        "time" => 1000, "host" => 'v', "interval" => 5,
-        "plugin" => 'v', "plugin_instance" => 'v',
-        "type" => 'v', "type_instance" => 'v',
+      d.emit({
+        "time" => 1000, "host" => 'host_v', "interval" => 5,
+        "plugin" => 'plugin_v', "plugin_instance" => 'plugin_instance_v',
+        "type" => 'type_v', "type_instance" => 'type_instance_v',
         "values" => ['v1', 'v2'], "dsnames" => ['n1', 'n2'], "dstypes" => ['t1', 't2']
-      }])
+      })
     end
 
-    assert_equal ({'n1'=>'v1','n2'=>'v2'} - d.records[0]).empty?, true
+    assert_equal d.records[0][:collectd]['plugin_v']['type_v']['n1'], 'v1'
+    assert_equal d.records[0][:collectd]['plugin_v']['type_v']['n2'], 'v2'
+    assert_equal d.records[0][:collectd].has_key?('host'), false
+    assert_equal d.records[0][:collectd]['plugin'], 'plugin_v'
+    assert_equal d.records[0][:collectd]['type'], 'type_v'
+    assert_equal d.records[0][:collectd]['plugin_instance'], 'plugin_instance_v'
+    assert_equal d.records[0][:collectd]['type_instance'], 'type_instance_v'
+    assert_equal d.records[0][:collectd]['dstypes'], 't1'
+
   end
 
 end
